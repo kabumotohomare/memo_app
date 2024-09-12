c306ba6864a17430e252c79901975be778006570
kabumotohomare <kabuhoma0506@gmail.com>
2024-09-12 10:51:33 +0900

    削除機能修正

--- resources/views/memos/show.blade.php
+++ resources/views/memos/show.blade.php
@@ -10,15 +10,22 @@
 </head>
 
 <body>
-    <p><a href ={{ route('memos.index') }}>戻る</a></p>
+    <p><a href={{ route('memos.index') }}>戻る</a></p>
     <h1>{{ $memo->title }}</h1>
     <P>{!! nl2br(e($memo->body)) !!}</P>
     <div class="button-group">
-        <button onclick='location.href="{{ route("memos.edit", $memo) }}"'>編集する</button>
+        <button onclick='location.href="{{ route('memos.edit', $memo) }}"'>編集する</button>
+
         <form action="{{ route('memos.destroy', $memo) }}" method="post">
-        @csrf
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+
+            {{-- @csrf
         @method('DELETE')
-        {{-- onclickでメッセージ出るのはHTMLに元からある機能 --}}
+        onclickでメッセージ出るのはHTMLに元からある機能 --}}
+
+        </form>
     </div>
 </body>
 
