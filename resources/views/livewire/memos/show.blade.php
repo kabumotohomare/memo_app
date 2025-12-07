dabe9979a830e6f41bfc7fe9f21b0bd7bcbfb3dc
kabumotohomare <kabuhoma0506@gmail.com>
2025-12-07 10:23:23 +0900

    詳細画面と一覧画面完成

---
+++ resources/views/livewire/memos/show.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use function Livewire\Volt\{state};
+use App\Models\Memo;
+//
+state(['memo' => fn(Memo $memo) => $memo]);
+?>
+
+<div>
+    <p><a href={{route('memos.index')}}>戻る</a></p>
+    <h1>{{ $memo->title }}</h1>
+    <p>{!! nl2br(e($memo->body))  !!}</p>
+</div>
