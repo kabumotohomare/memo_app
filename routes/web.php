8cfe4408ff2a3b3866e958727cc0b981c6faeb8d
kabumotohomare <kabuhoma0506@gmail.com>
2025-12-07 11:54:03 +0900

    登録機能完成

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//ルーティングの順番は固定ページが先、パラメーターつきページが後
 //一覧ページ
 Volt::route('/memos/', 'memos.index')->name('memos.index');
+
+//新規作成ページ
+Volt::route('/memos/create', 'memos.create')->name('memos.create');
 
 //詳細ページ
 Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
